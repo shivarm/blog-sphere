96180bf7cadca1fb8e5f22b9d525da30e76fda42
Shivam Sharma <meshivam81@gmail.com>
2025-03-22 21:53:40 +0530

    feat: add post list page

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from "react";
+import { PostList } from "../components/PostList";
+import { SideMenu } from "../components/SideMenu";
 
 const PostListPage = () => {
-  return (
-    <div>PostListPage</div>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default PostListPage
+  return (
+    <div className="">
+      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <button
+        onClick={() => setOpen((prev) => !prev)}
+        className="bg-blue-800 rounded-xl px-2 py-1 mb-4 md:hidden"
+      >
+        {open ? "Close" : "Filter or Search"}
+      </button>
+      <div className=" flex flex-col-reverse gap-8 md:flex-row">
+        <div className="">
+          <PostList />
+        </div>
+        <div className={`${open ? "block" : "hidden"} md:block`}>
+          <SideMenu />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostListPage;
